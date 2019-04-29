7662b51e4aecedea03930869fbb6c74a540afbdd
Fariz <fiagusta@gmail.com>
2019-04-29 17:15:18 +0700

    adding JWT

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/login', function (Request $request) {
+    $token = app('auth')->attempt($request->only('email', 'password'));
+ 
+    return response()->json(compact('token'));
+});
